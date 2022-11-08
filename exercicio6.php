e1900d4a1793cab96b6df95c1fb101f3ae9fd376
AulerLucas <lucasaulerbonfante@yahoo.com.br>
2022-11-08 20:13:59 -0300

    :zap:

---
+++ exercicio6.php
@@ -0,0 +1,21 @@
+<?php
+$valorHora = $_GET['vHora'];
+$mesHora = $_GET['vMes'];
+
+$salBruto = $mesHora * $valorHora; $inss = 0; 
+if ($salBruto < 1212.00) { 
+    $inss = ($salBruto * 0.075); 
+} else if ($salBruto >= 1212.01 && $salBruto <= 2427.35) { 
+        $inss = ($salBruto * 0.09); 
+    } else if ($salBruto >= 2457.36 && $salBruto <= 3641.03) {
+             $inss = ($salBruto * 0.12); 
+            } else { $inss = ($salBruto * 0.14); } 
+             
+             $salLiquido = $salBruto - $inss; 
+
+
+
+echo "Salário Bruto: $salBruto <br>
+<br> Desconto INSS: $inss <br>
+<br> Salário Líquido: $salLiquido ";
+?>
---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <title> exercicio6 </title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="style.css">
+ 
+    </head>
+    <body>
+        <form action="exercicio6.php" method="get">
+            <p id="vHora">
+            Informe o valor da hora de aula:  <input type="number" step='0.01' name="vHora" />
+            </p>
+            <p id="vMes">
+                Informe o número de horas trabalhadas no mês:  <input type="number" step='0.01' name="vMes"  />
+            </p>
+    
+<input type="submit" value="Calcular" name='calc' id="calc">
+        </form>
+    </body>
+</html>
